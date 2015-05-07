41f9f622142835c336f524257513a45a9d6374ad
Jessy Riordan <jessy.riordan@gmail.com>
2015-05-07 16:14:26 -0400

    Green at 3

---
+++ string-calculator.js
@@ -0,0 +1,21 @@
+var expect = require("chai").expect;
+
+function toNumber(word){
+  if ( word === "zero" ){
+    return 0;
+  }
+  if ( word === "one" ){
+    return 1;
+  }
+  if ( word === "two" ){
+    return 2;
+  }
+  if ( word === "three" ){
+    return 3;
+  }
+}
+
+expect(toNumber("zero")).to.equal(0);
+expect(toNumber("one")).to.equal(1);
+expect(toNumber("two")).to.equal(2);
+expect(toNumber("three")).to.equal(3);
