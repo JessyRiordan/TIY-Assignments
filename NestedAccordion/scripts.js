a12c6777b73680077914e470957e2b33d80a81c2
Jessy Riordan <jessy.riordan@gmail.com>
2015-05-12 19:41:35 -0400

    I am getting clicks!!

---
+++ NestedAccordion/scripts.js
@@ -0,0 +1,25 @@
+
+
+document.head.parentElement.className = "js";
+
+var elements = document.querySelectorAll('h3');
+
+// elements[0].addEventListener('click', function(){
+// elements[0].parentElement.classList.toggle('cbp-ntopen');
+// });
+
+// for ( var index = 0; index < elements.length; index++ ){
+//   var element = elements[index];
+//   element[index].addEventListener('click', function(){
+//     console.log(elements, index, elements[index]);
+//     element[index].parentElement.classList.toggle('cbp-ntopen');
+//   });
+// }
+
+
+_.forEach(elements, function(element, index, elements){
+  element.addEventListener('click', function(){
+    console.log(elements, index, element);
+    element.parentElement.classList.toggle('cbp-ntopen');
+  });
+});
