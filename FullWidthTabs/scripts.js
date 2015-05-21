1e7ef010df17058177ea6c281ce3be7d94a17bd1
Jessy Riordan <jessy.riordan@gmail.com>
2015-05-21 18:32:01 -0400

    work on fullwidthtabs

--- FullWidthTabs/scripts.js
+++ FullWidthTabs/scripts.js
@@ -1,19 +1,22 @@
-// alert("Hello, I am working!");
+(function(window){
 
-document.head.parentElement.className = "js";
+})(window);
 
-var tabElements = document.querySelectorAll('a');
 
-var panelElements = document.querySelectorAll('h3');
-
-_.forEach(tabElements, function(element, index, tabElements){
-  element.addEventListener('click', function(index){
-    element.parentElement.classList.toggle('tab-current');
-
-  });
-
-  _.forEach(panelElements, function(element, index, panelElements){
-    element.addEventListener('click', function(index){
-      element.parentElement.classList.toggle('content-current');
-    });
-});
+// document.head.parentElement.className = "js";
+//
+// var tabElements = document.querySelectorAll('a');
+//
+// var panelElements = document.querySelectorAll('h3');
+//
+// _.forEach(tabElements, function(element, index, tabElements){
+//   element.addEventListener('click', function(index){
+//     element.parentElement.classList.toggle('tab-current');
+//
+//   });
+//
+//   _.forEach(panelElements, function(element, index, panelElements){
+//     element.addEventListener('click', function(index){
+//       element.parentElement.classList.toggle('content-current');
+//     });
+// });
